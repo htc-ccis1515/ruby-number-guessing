6a1b263485ae3a9d8ba33f89f53b12fee4881c21
mbMosman <mbmosman@users.noreply.github.com>
2016-10-25 06:27:48 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ruby-number-guessing
+
+## Number Guessing Game
+We're going to write a number guessing game in ruby.  
+
+The game should:
+
+- Ask for the player's name
+- Greet the player by name and explain the game
+- Ask for the maximum number to select as the secret number
+- Pick a secret number at random between 1 and the number entered
+- Ask the player (by name) to guess a number
+- Tell the player if they guessed correctly
+- If they guessed the number correctly, congratulate them for winning and then tell them how many guesses it took for them to win.
+- If they did not guess correctly, tell them whether their guess was too high or too low.
+
+Make sure to build methods to help break down the problem and make the code easier to read.
---
+++ guessing-game.rb
@@ -0,0 +1,19 @@
+##############################################################################
+#
+# Update this script to:
+#
+# - Ask for the player's name
+# - Greet the player by name and explain the game
+# - Ask for the maximum number to select as the secret number
+# - Pick a secret number at random between 1 and the number entered
+# - Ask the player (by name) to guess a number
+# - Tell the player if they guessed correctly
+# - If they guessed the number correctly, congratulate them for winning
+#   and then tell them how many guesses it took for them to win.
+# - If they did not guess correctly, tell them whether their guess was
+#   too high or too low.
+#
+# DON'T FORGET:
+#   Use methods to break down the problem and make the code easier to read.
+#
+#############################################################################
